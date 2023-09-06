6b757629f93cfcbc5c8c4586b9dea84fcb1421e6
weiraneve <907221539@qq.com>
2023-09-06 22:45:02 +0800

    refactor: move navigation page

--- lib/common/navigation/routes.dart
+++ lib/common/navigation/routes.dart
@@ -1,6 +1,6 @@
 import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:seckill_deal/common/auth/auth_middleware.dart';
-import 'package:seckill_deal/pages/goods/list/view/navigation_page.dart';
+import 'package:seckill_deal/component/navigation/navigation_page.dart';
 import 'package:seckill_deal/pages/login/view/login_page.dart';
 import 'package:seckill_deal/pages/register/view/register_page.dart';
 
