591d33a3bb8ce76af35f61ac8f36ec9faef2b749
weiraneve <907221539@qq.com>
2023-09-19 21:07:45 +0800

    refactor: extract getErrorMessage

--- lib/common/utils/network_util.dart
+++ lib/common/utils/network_util.dart
@@ -5,7 +5,7 @@ import 'package:seckill_deal/common/logger.dart';
 import 'package:seckill_deal/res/strings.dart';
 
 class NetworkUtil {
-  static void handleError(Object e) {
+  static String _getErrorMessage(Object e) {
     String errorMessage = stringRes(R.unknownError);
     if (e is DioException) {
       final response = e.response;
@@ -15,20 +15,18 @@ class NetworkUtil {
         errorMessage = response.data['msg'] ?? errorMessage;
       }
     }
+    return errorMessage;
+  }
+
+  static void handleError(Object e) {
+    _getErrorMessage(e);
     logger.e(e);
   }
 
   static String handleErrorMessage(Object e) {
-    String errorMessage = stringRes(R.unknownError);
-    if (e is DioException) {
-      final response = e.response;
-      if (e.error is SocketException) {
-        errorMessage = stringRes(R.networkError);
-      } else if (response != null && response.data is Map) {
-        errorMessage = response.data['msg'] ?? errorMessage;
-      }
-    }
+    String errorMessage = _getErrorMessage(e);
     logger.e(e);
     return errorMessage;
   }
 }
+
