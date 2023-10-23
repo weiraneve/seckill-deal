21cabf931720f1e97ad1044ff70c985e2518e40c
weiraneve <907221539@qq.com>
2023-10-23 23:41:10 +0800

    feat: add disposed filed prevent async error

--- lib/common/view_model/base_view_model.dart
+++ lib/common/view_model/base_view_model.dart
@@ -3,11 +3,25 @@ import 'package:seckill_deal/common/auth/state.dart';
 
 class BaseViewModel extends ChangeNotifier {
   AuthState _state = const AuthInitial();
+  bool _disposed = false;
 
   AuthState get state => _state;
 
   set state(AuthState state) {
     _state = state;
     notifyListeners();
+  }
+
+  @override
+  void notifyListeners() {
+    if (!_disposed) {
+      super.notifyListeners();
+    }
+  }
+
+  @override
+  void dispose() {
+    _disposed = true;
+    super.dispose();
   }
 }
